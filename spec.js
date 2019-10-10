553eca8766b07d630705b6d87a5fb167f130152f
cruzm430 <cruzm430@gmail.com>
2019-10-10 19:19:48 -0400

    added express

--- spec.js
+++ spec.js
@@ -1,7 +1,18 @@
 const {expect} = require('chai')
+const app = require('supertest')(require('./app'));
+
+let woo = false
+woo = !woo
 
 describe('Our Application', ()=>{
   it('true is true', ()=>{
-    expect(true).to.equal(true)
+    expect(woo).to.equal(true)
+  })
+  describe('GET /', ()=>{
+    it('returns a 200 with foo', async ()=>{
+      const response = await app.get('/')
+      expect(response.status).to.equal(200)
+      expect(response.text).to.contain('foo')
+    })
   })
 })
